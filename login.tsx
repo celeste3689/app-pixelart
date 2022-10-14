7a5c4e4c9ac76e619d536c5b4cdd2ff49752ad7a
Celeste <celestetorres@epet12smandes.edu.ar>
2022-10-14 09:47:50 -0300

    algo

---
+++ login.tsx
@@ -0,0 +1,9 @@
+<script>
+<div>
+    email: <input type="text" bind:value={state.email} name="" />
+    <br/>
+    password: <input type="text" bind:value={state.password} name=""></br>
+    
+    <button on:click={()=>} >login now</button>
+</div>
+</script>
