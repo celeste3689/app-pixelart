c1b8532c5b0fc05da7b9870c33de52efa97a32fa
Dani <danieltrecaman@gmail.com>
2022-11-23 08:23:15 -0300

    creacion de las vistas y estilos de el panel de dibujo

--- app-pixelart/src/App.tsx
+++ app-pixelart/src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './views/Login';
+import DimensionView from './views/dimension-view/DimensionView';
+import DrawView from './views/draw-view/DrawView';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}/>
+          <Route path='/dimension-view' element={<DimensionView/>}/>
+          <Route path='/draw-view' element={<DrawView/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
